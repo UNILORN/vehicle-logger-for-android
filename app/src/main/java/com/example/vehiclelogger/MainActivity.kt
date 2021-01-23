93999f46be73dc8ebb3bde0c87d93885e60a3ef8
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2021-01-23 19:23:37 +0900

    GPS Location Data

--- app/src/main/java/com/example/vehiclelogger/MainActivity.kt
+++ app/src/main/java/com/example/vehiclelogger/MainActivity.kt
@@ -1,49 +1,87 @@
 package com.example.vehiclelogger
 
+import android.Manifest
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.Location
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
-import net.openid.appauth.AuthorizationRequest
-import net.openid.appauth.AuthorizationService
-import net.openid.appauth.AuthorizationServiceConfiguration
-import net.openid.appauth.ResponseTypeValues
-
-private val ISSUER_URI = "https://auth.optim.cloud"
-private val CLIENT_ID = ""
-private val REDIRECT_URI = "net.paonejp.kndzyb.appauthdemo:/cb"
-
-private val SCOPE = "profile"
-private val API_URI = "https://www.googleapis.com/oauth2/v3/userinfo"
-
-private val REQCODE_AUTH = 100
-private val X_HTTP_NEED_REAUTHZ = -1
-private val X_HTTP_ERROR = -9
-
-private val LOG_TAG = "MainActivity"
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
+import net.openid.appauth.*
+import net.openid.appauth.AuthorizationServiceConfiguration.RetrieveConfigurationCallback
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-//    private fun startAuthorization() {
-//        AuthorizationServiceConfiguration.fetchFromIssuer(Uri.parse(ISSUER_URI), { config, ex ->
-//            if (config != null) {
-//                val req = AuthorizationRequest
-//                        .Builder(config, CLIENT_ID, ResponseTypeValues.CODE, Uri.parse(REDIRECT_URI))
-//                        .setScope(SCOPE)
-//                        .build()
-//                val intent = AuthorizationService(this).getAuthorizationRequestIntent(req)
-//                startActivityForResult(intent, REQCODE_AUTH)
-//            } else {
-//                if (ex != null) {
-//                    val m = Throwable().stackTrace[0]
-//                    Log.e(LOG_TAG, "${m}: ${ex}")
-//                }
-//                whenAuthorizationFails(ex)
-//            }
-//        })
-//    }
+        if (
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                {
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                        requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION),10)
+                    }
+                }
+
+        val locationRequest = LocationRequest.create().apply {
+            interval = 1000
+            fastestInterval = 500
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest)
+
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locationSettingsResponse ->
+
+        }
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException){
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(this@MainActivity,
+                            10)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+        
+
+        val lat_text = findViewById<TextView>(R.id.lat_text)
+        val lon_text = findViewById<TextView>(R.id.lon_text)
+        thread {
+            for (i in 1..100) {
+                fusedLocationClient.lastLocation
+                        .addOnSuccessListener { location: Location ->
+
+                            lat_text.setText( location.latitude.toString())
+                            lon_text.setText(location.longitude.toString())
+                        }
+                Thread.sleep(1000)
+            }
+        }
+
+    }
 }
